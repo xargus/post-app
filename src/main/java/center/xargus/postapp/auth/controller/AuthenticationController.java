b84b8277fa0fb162d3cec703cc7a217ba268abf5
kim.junhong <kim.junhong@navercorp.com>
2017-11-27 16:06:42 +0900

    localhost에서 테스트 가능하도록 수정

--- src/main/java/center/xargus/postapp/auth/controller/AuthenticationController.java
+++ src/main/java/center/xargus/postapp/auth/controller/AuthenticationController.java
@@ -12,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.google.gson.Gson;
 
@@ -27,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class AuthenticationController {
 	private Logger log = Logger.getLogger(this.getClass());
