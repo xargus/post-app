51f3b166e5b7a006a356913c1fa40cb2b7da4a2d
kim.junhong <kim.junhong@navercorp.com>
2018-02-02 14:21:19 +0900

    빌드 오류 수정

--- src/test/java/center/xargus/postapp/SqlTest.java
+++ src/test/java/center/xargus/postapp/SqlTest.java
@@ -1,19 +1,15 @@
 package center.xargus.postapp;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/application-config.xml")
 public class SqlTest {
-	
-	@Autowired
-	private AuthenticationDaoOld registerDao;
 	
 	@Test
 	public void test() {
